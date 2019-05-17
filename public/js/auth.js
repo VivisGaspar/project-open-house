8eee4766b6a509d8b2bd0f0afa07f97682ca3ab2
anacamargo <anaclaudia2501@gmail.com>
2019-05-17 18:57:01 -0300

    [firebase] inclusão do firebase e arquivo de autenticação

---
+++ public/js/auth.js
@@ -0,0 +1,75 @@
+var database = firebase.database();
+
+$(document).ready(function () {
+    $(".sign-up-button").click(signUpClick);
+    $(".sign-in-button").click(signInClick);
+});
+
+function signUpClick(event) {
+    event.preventDefault();
+
+    let name = $(".sign-up-name").val();
+    let birthday = $(".sign-up-birthday").val();
+    let gender = $(".sign-up-gender").val();
+    let state =  $(".sign-up-state").val();
+    let city =  $(".sign-up-city").val();
+    let email = $(".sign-up-email").val();
+    let password = $(".sign-up-password").val();
+
+    createUser(name, birthday, gender, state, city, email, password);
+}
+
+function signInClick(event) {
+    event.preventDefault();
+
+    var email = $(".sign-in-email").val();
+    var password = $(".sign-in-password").val();
+
+    loginUserAuth(email, password);
+}
+
+function createUser(name, birthday, gender, state, city, email, password) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(function (response) {
+            if (response.operationType === "signIn") {
+                var userId = response.user.uid;
+
+                createUserInDB(userId, name, birthday, gender, state, city, email);
+                signInRedirect(userId);
+            }
+        })
+        .catch(function (error) { handleError(error); });
+}
+
+function loginUserAuth(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function (response) {
+            if (response.operationType === "signIn") {
+                var userId = response.user.uid;
+                sessionStorage["USER_ID"] = userId;
+                signInRedirect(userId);
+            }
+        })
+        .catch(function (error) { handleError(error); });
+}
+
+function createUserInDB(id, name, birthday, gender, state, city, email) {
+    database.ref('users/' + id).set({
+        name: name,
+        birthday: birthday,
+        gender: gender,
+        state: state,
+        city: city,
+        email: email,
+        picture: 'images/avatar.png'
+    });
+}
+
+function signInRedirect(userId) {
+    window.location = '../home.html';
+}
+
+function handleError(error) {
+    alert(error.message);
+    console.log(error.code, error.message);
+}
