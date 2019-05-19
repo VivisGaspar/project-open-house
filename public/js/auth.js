c3d60538b3ad3e561a75e7b361f8bd8a6fc69740
anacamargo <anaclaudia2501@gmail.com>
2019-05-18 21:41:30 -0300

    [master] resolvendo conflito

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,57 +1,57 @@
-// var database = firebase.database();
+var database = firebase.database();
 
-// $(document).ready(function() {
-//   $(".sign-up-button").click(signUpClick);
-//   $(".sign-in-button").click(signInClick);
-// });
+$(document).ready(function() {
+  $(".sign-up-button").click(signUpClick);
+  $(".sign-in-button").click(signInClick);
+});
 
-// function signUpClick(event) {
-//   event.preventDefault();
+function signUpClick(event) {
+  event.preventDefault();
 
-//   let name = $(".sign-up-name").val();
-//   let birthday = $(".sign-up-birthday").val();
-//   let gender = $(".sign-up-gender").val();
-//   let state = $(".sign-up-state").val();
-//   let city = $(".sign-up-city").val();
-//   let email = $(".sign-up-email").val();
-//   let password = $(".sign-up-password").val();
+  let name = $(".sign-up-name").val();
+  let birthday = $(".sign-up-birthday").val();
+  let gender = $(".sign-up-gender").val();
+  let state = $(".sign-up-state").val();
+  let city = $(".sign-up-city").val();
+  let email = $(".sign-up-email").val();
+  let password = $(".sign-up-password").val();
 
-//   createUser(name, birthday, gender, state, city, email, password);
-// }
+  createUser(name, birthday, gender, state, city, email, password);
+}
 
-// function signInClick(event) {
-//   event.preventDefault();
+function signInClick(event) {
+  event.preventDefault();
 
-//   var email = $(".sign-in-email").val();
-//   var password = $(".sign-in-password").val();
+  var email = $(".sign-in-email").val();
+  var password = $(".sign-in-password").val();
 
-//   loginUserAuth(email, password);
-// }
+  loginUserAuth(email, password);
+}
 
-// function createUser(name, birthday, gender, state, city, email, password) {
-//   firebase.auth().createUserWithEmailAndPassword(email, password)
-//     .then(function(response) {
-//       if (response.operationType === "signIn") {
-//         var userId = response.user.uid;
+function createUser(name, birthday, gender, state, city, email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(response) {
+      if (response.operationType === "signIn") {
+        var userId = response.user.uid;
 
-//         createUserInDB(userId, name, birthday, gender, state, city, email);
-//         signInRedirect(userId);
-//       }
-//     })
-//     .catch(function(error) { handleError(error); });
-// }
+        createUserInDB(userId, name, birthday, gender, state, city, email);
+        signInRedirect(userId);
+      }
+    })
+    .catch(function(error) { handleError(error); });
+}
 
-// function loginUserAuth(email, password) {
-//   firebase.auth().signInWithEmailAndPassword(email, password)
-//     .then(function(response) {
-//       if (response.operationType === "signIn") {
-//         var userId = response.user.uid;
-//         sessionStorage["USER_ID"] = userId;
-//         signInRedirect(userId);
-//       }
-//     })
-//     .catch(function(error) { handleError(error); });
-// }
+function loginUserAuth(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(response) {
+      if (response.operationType === "signIn") {
+        var userId = response.user.uid;
+        sessionStorage["USER_ID"] = userId;
+        signInRedirect(userId);
+      }
+    })
+    .catch(function(error) { handleError(error); });
+}
 
 function createUserInDB(id, name, birthday, gender, state, city, email) {
   database.ref('users/' + id).set({
@@ -65,23 +65,23 @@ function createUserInDB(id, name, birthday, gender, state, city, email) {
   });
 }
 
-// function signInRedirect(userId) {
-//   window.location = '../index.html';
-// }
+function signInRedirect(userId) {
+  window.location = '../index.html';
+}
 
-// function handleError(error) {
-//   alert(error.message);
-//   console.log(error.code, error.message);
-// }
+function handleError(error) {
+  alert(error.message);
+  console.log(error.code, error.message);
+}
 
-// function Liststate(){
-//   let state = $(".sign-up-state");
-//   return state;
-// }
+function Liststate(){
+  let state = $(".sign-up-state");
+  return state;
+}
 
-// function ListCity(){
-//   let city = $(".sign-up-city").val();
-// }
+function ListCity(){
+  let city = $(".sign-up-city").val();
+}
 
 function getStateCityonApi() {
   let estado = "";
