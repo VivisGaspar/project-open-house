1d5417967151112cc4d80fbfd185241bb3d9a43f
nayamarosa <falecom@nayama.com.br>
2019-05-20 17:07:42 -0300

    [css][html][js]começo de aplicação de estilo para media acima de 768px,ideia de como pegar dados do firebase para o match

--- public/js/match.js
+++ public/js/match.js
@@ -1,4 +1,4 @@
-const element = document.getElementById("photo")
+const element = document.getElementById("match-image")
 
 let index = 0;
 
@@ -7,7 +7,6 @@ function getPhotoFromDB(){
 	fetch(url)
 	.then(resp => resp.json())
 	.then(resp => getDataFromBD(resp.data))
-	
 }
 
 function getDataFromBD(data){
@@ -25,3 +24,53 @@ hammertime.on('swipe', function(ev) {
 
 
 getPhotoFromDB()
+
+
+// const database = firebase.database();
+
+// let USER_ID = sessionStorage["USER_ID"];
+// if (!USER_ID) window.location.href = "login.html";
+
+// const image = document.getElementById("match-image")
+// const about = document.getElementById("match-about")
+
+// let index = 0;
+
+// function getPhotoFromDB(){
+// 	const url = "https://api.giphy.com/v1/gifs/trending?&api_key=7Mc3xZd6KslRRtCtWe72NCyO1HeKYRks" 
+// 	fetch(url)
+// 	.then(resp => resp.json())
+// 	.then(resp => getDataFromBD(resp.data))
+// }
+
+// function getDataFromBD(data){
+// 	const image = data[index].picture;
+// 	image.innerHTML = `<img src="${image}">`
+
+// 	const name = data[index].name;
+// 	const age = data[index].age;
+// 	const about = data[index].about;
+// 	const activities = data[index].activities;
+
+// 	about.innerHTML = `
+// 	<p class="name-age font-size-g">${name}, ${age}</p>
+// 	<p class="about-title font-size-p">SOBRE${name}</p>
+// 	<p class="text-about font-size-p">${about}</p>
+// 	<section class="activities">
+// 		<p class="about-title font-size-p">ATIVIDADES</p>
+// 		<div>${activities}</div>
+// 	</section>
+// 	`
+// 	index++;
+// 	console.log(imgUrl);
+// 	// preciso pegar a foto, distância, nome, idade, resumo e atividades   
+// }
+
+// const hammertime = new Hammer(element);
+// hammertime.on('swipe', function(ev) {
+// 	console.log(ev);
+// 	getPhotoFromDB();
+// });
+
+
+// getPhotoFromDB()
