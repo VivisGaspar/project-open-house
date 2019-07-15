b5e4ea9ca2d2b0f946d794e7f3e9f685e03cbf51
anacamargo <anaclaudia2501@gmail.com>
2019-07-15 18:43:39 -0300

    inclusão dos métodos de salvar e de trazer as atividades

--- public/js/Repository.js
+++ public/js/Repository.js
@@ -24,7 +24,33 @@ class Repository {
 		return user;
 	}
 
+	async getActivityByID(id, userID) {
+    const ref = this.database.ref('activities/' + userID + '/' + id);
+    const snap = await ref.once('value');
+    const key = snap.key;
+    const activity = snap.val();
+    activity['id'] = key;
+    return activity;
+	}
+
+	async getActivitiesByUserId(userId) {
+    const ref = database.ref('activities/' + userId);
+    const snaps = await ref.once('value');
+    let activities = [];
+    snaps.forEach((child) => {
+      let activity = child.val();
+      activity['id'] = child.key;
+      activities.push(activity);
+    });
+    return activities.reverse();
+  }
+
 	updateUser(id, newUser, userID) {
 		this.database.ref('users/' + (userID || USER_ID)).update(newUser);
+	}
+
+	insertActivity(activity){
+		const message = this.database.ref('activities/' + USER_ID).push(activity);
+    return message.key;
 	}
 }
